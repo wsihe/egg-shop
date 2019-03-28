ae1a2c60dcabff3505666ac3ed7eb9da7b67430e
river <837606100@qq.com>
2019-03-28 18:10:20 +0800

    [egg-shop] add graphql

--- config/config.default.js
+++ config/config.default.js
@@ -16,7 +16,7 @@ module.exports = appInfo => {
   config.keys = appInfo.name + '_1553162287916_6297';
 
   // add your middleware config here
-  config.middleware = ['errorHandler'];
+  config.middleware = ['errorHandler', 'graphql'];
 
   config.news = {
     pageSize: 30,
@@ -36,6 +36,18 @@ module.exports = appInfo => {
     // 是否加载到 agent 上，默认关闭
     agent: false,
   };
+
+	config.graphql = {
+		router: '/graphql',
+		app: true,
+		agent: false,
+		// 是否加载开发者工具 graphiql, 默认开启。路由同 router 字段。使用浏览器打开该可见。
+		graphiql: true,
+		// graphQL 路由前的拦截器
+		onPreGraphQL: function* (ctx) {},
+		// 开发工具 graphiQL 路由前的拦截器，建议用于做权限操作(如只提供开发者使用)
+		onPreGraphiQL: function* (ctx) {},
+	};
 
   // add your user config here
   const userConfig = {
--- config/plugin.js
+++ config/plugin.js
@@ -13,5 +13,9 @@ module.exports = {
   mysql: {
     enable: true,
     package: 'egg-mysql',
-  }
+  },
+	graphql: {
+		enable: true,
+		package: 'egg-graphql',
+	}
 };
